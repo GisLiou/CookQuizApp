72376962dffbff2087e912aa429a86158bd87be5
Gary <Gary.Liu@mail.dsic.com.tw>
2026-08-12 16:03:35 +0800

    feat: 配合 GitHub Page 需要修正參數

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react()],
+  base: '/CookQuizApp/', // 務必確保與你的 Repository 名稱大小寫完全一致
 })
